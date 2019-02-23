9719859fa036c8df483076ecaa61fed86deab48a
Sarmitha <sarmicbe@gmail.com>
2019-02-23 18:08:07 +0530

    health care system

---
+++ js/main.js
@@ -0,0 +1,111 @@
+
+$(document).ready(function(){
+	"use strict";
+
+	var window_width 	 = $(window).width(),
+	window_height 		 = window.innerHeight,
+	header_height 		 = $(".default-header").height(),
+	header_height_static = $(".site-header.static").outerHeight(),
+	fitscreen 			 = window_height - header_height;
+
+
+	$(".fullscreen").css("height", window_height)
+	$(".fitscreen").css("height", fitscreen);
+     $(".default-header").sticky({topSpacing:0});
+
+ 
+     $('select').niceSelect();
+     
+     
+   
+
+  $(".menu-bar").on('click', function(e){
+      e.preventDefault();
+      $("nav").toggleClass('hide');
+      $("span", this).toggleClass("lnr-menu lnr-cross");
+      $(".main-menu").addClass('mobile-menu');
+  });
+
+
+  $('.nav-item a:first').tab('show');
+
+
+
+ 
+  $('.main-menubar a[href*="#"]')
+
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+      
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        
+        if (target.length) {
+         
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top-68
+          }, 1000, function() {
+            
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { 
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); 
+              $target.focus(); 
+            };
+          });
+        }
+      }
+    });
+
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    
+
+      // -------   Mail Send ajax
+
+         $(document).ready(function() {
+            var form = $('#myForm'); // contact form
+            var submit = $('.submit-btn'); // submit button
+            var alert = $('.alert-msg'); // alert div for show alert message
+
+            // form submit event
+            form.on('submit', function(e) {
+                e.preventDefault(); // prevent default form submit
+
+                $.ajax({
+                    url: 'mail.php', // form action url
+                    type: 'POST', // form submit method get/post
+                    dataType: 'html', // request type html/json/xml
+                    data: form.serialize(), // serialize form data
+                    beforeSend: function() {
+                        alert.fadeOut();
+                        submit.html('Sending....'); // change submit button text
+                    },
+                    success: function(data) {
+                        alert.html(data).fadeIn(); // fade in response data
+                        form.trigger('reset'); // reset form
+                        submit.attr("style", "display: none !important");; // reset submit button text
+                    },
+                    error: function(e) {
+                        console.log(e)
+                    }
+                });
+            });
+        });
+
+
+ });
